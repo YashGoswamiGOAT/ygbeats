7fc1ad096bfdbcaaabbbe85961f94c9fed5be746
YashGoswamiGOAT <124524109+YashGoswamiGOAT@users.noreply.github.com>
2023-02-14 10:29:12 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from flask import Flask
 import json
 from bs4 import BeautifulSoup
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/getMusic/<name>")
 def music(name):
